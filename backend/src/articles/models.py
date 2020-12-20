46d65ae72f5553bffcafad236fefcd1725a9b215
cryptDecoder <pruthvirajs2007@gmail.com>
2020-12-20 13:29:58 +0530

    API done

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(blank=False, max_length=50, null=False)
+    content = models.TextField(max_length=100)
+
+    def __str__(self):
+        return "{:s}".format(self.title)
--- backend/src/articles/views.py
+++ backend/src/articles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Article
+from .serializers import ArticleSerializer
+
 
 # Create your views here.
+class ArtileListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
